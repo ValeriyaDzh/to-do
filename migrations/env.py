e7578959c47c228dc053345d50f10fc1cd706262
Valeriya <Dzhavadova.v@yandex.ru>
2024-08-07 15:04:50 +0300

    add task model

--- migrations/env.py
+++ migrations/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from src.users.models import User
+from src.tasks.models import Task
 from src.database import Base
 from src.config import settings
 
