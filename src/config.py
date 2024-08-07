de484b736097f1255e291386da4d90ca286a7241
Valeriya <Dzhavadova.v@yandex.ru>
2024-08-07 14:29:24 +0300

    add logger settings

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+import logging
+from logging.config import dictConfig
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
@@ -23,9 +25,56 @@ class DatabaseSettings(BaseSettings):
         )
 
 
+class LoggerSettings(BaseSettings):
+
+    LEVEL: str
+    FILE: str
+    IGNORED_LOGGERS: list[str] = ["passlib", "asyncio"]
+    IGNORED_LOGGERS_LEVEL: str = "ERROR"
+
+    model_config = SettingsConfigDict(env_prefix="LOG_", extra="ignore")
+
+    def configure_logging(self):
+        dictConfig(
+            {
+                "version": 1,
+                "disable_existing_loggers": False,
+                "formatters": {
+                    "default": {
+                        "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                        "datefmt": "%Y-%m-%d %H:%M:%S",
+                    },
+                },
+                "handlers": {
+                    "file": {
+                        "level": self.LEVEL,
+                        "formatter": "default",
+                        "class": "logging.FileHandler",
+                        "filename": self.FILE,
+                    },
+                },
+                "loggers": {
+                    "": {
+                        "handlers": ["file"],
+                        "level": self.LEVEL,
+                        "propagate": False,
+                    },
+                },
+            }
+        )
+
+        for log in self.IGNORED_LOGGERS:
+            self._set_level(log, self.IGNORED_LOGGERS_LEVEL)
+
+    @staticmethod
+    def _set_level(logger: str, level: str):
+        logging.getLogger(logger).setLevel(level)
+
+
 class Settings(BaseSettings):
 
     db: DatabaseSettings = DatabaseSettings()
+    log: LoggerSettings = LoggerSettings()
 
 
-config = Settings()
+settings = Settings()
