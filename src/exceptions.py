fb55d7002cef1a8a14530945af1329c05e219fe9
Valeriya <Dzhavadova.v@yandex.ru>
2024-08-07 18:22:02 +0300

    add UnprocessableException

--- src/exceptions.py
+++ src/exceptions.py
@@ -3,7 +3,15 @@ from fastapi import HTTPException, status
 
 class DatabaseException(HTTPException):
 
-    def __init__(self, detail="Server error...Please try again later") -> None:
+    def __init__(self, detail="Server error...Please try again later"):
         super().__init__(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=detail
+        )
+
+
+class UnprocessableException(HTTPException):
+
+    def __init__(self, detail="Validation error"):
+        super().__init__(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=detail
         )
