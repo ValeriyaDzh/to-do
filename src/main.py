cb05e750a1e79c8d37fa4d1474d1d8be0aeca940
Valeriya <Dzhavadova.v@yandex.ru>
2024-08-07 18:21:33 +0300

    add users_router

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 from src.config import settings
+from src.users.routers import users_router
 
 settings.log.configure_logging()
 
 app = FastAPI(title="ToDo")
+
+app.include_router(users_router)
