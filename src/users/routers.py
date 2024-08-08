c19ff09769d87a36145f0ffb0b4633cc935e2a49
Valeriya <Dzhavadova.v@yandex.ru>
2024-08-08 15:58:53 +0300

    add sign-in endpoint

--- src/users/routers.py
+++ src/users/routers.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, status, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 
-from src.users.dependencies import get_user_service
-from src.users.schemas import CreateUser, ShowUser
+from src.exceptions import InvalidCredentialsException
+from src.users.auth import JWTToken, Password
+from src.users.dependencies import get_user_service, get_current_user
+from src.users.schemas import CreateUser, ShowUser, Token
 from src.users.services import UserService
 
 users_router = APIRouter(tags=["User"])
@@ -14,3 +17,19 @@ async def create_user(
     user_data: CreateUser, user_service: UserService = Depends(get_user_service)
 ):
     return await user_service.create(user_data)
+
+
+@users_router.post("/sign-in", status_code=status.HTTP_200_OK, response_model=Token)
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    user_service: UserService = Depends(get_user_service),
+):
+    user = await user_service.get_by_login(form_data.username)
+
+    if user and Password.verify(form_data.password, user.hashed_password):
+        access_token = JWTToken.create_access_token({"sub": user.login})
+
+        return Token(access_token=access_token, token_type="bearer")
+
+    else:
+        raise InvalidCredentialsException("Incorrect login or password")
