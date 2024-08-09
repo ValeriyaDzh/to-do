bc04805c90f61f5cec1c22e0ee6c3322807db650
Valeriya <Dzhavadova.v@yandex.ru>
2024-08-09 13:50:44 +0300

    create PermissionService

--- src/tasks/services.py
+++ src/tasks/services.py
@@ -1,12 +1,12 @@
 import logging
 from uuid import UUID
 
-from sqlalchemy import update
+from sqlalchemy import update, select, insert, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.exceptions import DatabaseException
+from src.exceptions import DatabaseException, AlreadyExistsException, NotFoundException
 from src.repositories import BaseRepository
-from src.tasks.models import Task
+from src.tasks.models import Task, task_permissions
 from src.tasks.schemas import CreateTask, UpdateTask
 
 
@@ -16,6 +16,7 @@ logger = logging.getLogger(__name__)
 class TaskService(BaseRepository):
 
     def __init__(self, session: AsyncSession):
+        self.permission = PermissionService(session=session)
         super().__init__(model=Task, session=session)
 
     async def create(self, data: CreateTask, author: UUID) -> Task:
@@ -57,3 +58,69 @@ class TaskService(BaseRepository):
 
     async def remove(self, id: UUID):
         await self.delete(id)
+
+
+class PermissionService:
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get(self, task_id: UUID, user_id: UUID, permission: str):
+
+        try:
+            result = await self.session.execute(
+                select(task_permissions).where(
+                    task_permissions.c.task_id == task_id,
+                    task_permissions.c.user_id == user_id,
+                    task_permissions.c.permission == permission,
+                )
+            )
+
+            return result.fetchone()
+
+        except Exception as e:
+            logger.error(f"Error get permission: {e}")
+            raise DatabaseException
+
+    async def add(self, task_id: UUID, user_id: UUID, permission: str):
+        existing_permission = await self.get(task_id, user_id, permission)
+
+        if existing_permission:
+            raise AlreadyExistsException(f"This permission for user already exists.")
+
+        try:
+            edded_permission = await self.session.execute(
+                insert(task_permissions).values(
+                    task_id=task_id, user_id=user_id, permission=permission
+                )
+            )
+            await self.session.commit()
+            logger.info(
+                f"Granted permission {permission} to user {user_id} for task {task_id}."
+            )
+            return edded_permission
+
+        except Exception as e:
+            logger.error(f"Error add permission: {e}")
+            raise DatabaseException
+
+    async def delete(self, task_id: UUID, user_id: UUID, permission: str):
+
+        existing_permission = self.get(task_id, user_id, permission)
+
+        if not existing_permission:
+            raise NotFoundException("Permission for user not found")
+
+        try:
+            await self.session.execute(
+                delete(task_permissions).where(
+                    task_permissions.c.task_id == task_id,
+                    task_permissions.c.user_id == user_id,
+                    task_permissions.c.permission == permission,
+                )
+            )
+            await self.session.commit()
+
+        except Exception as e:
+            logger.error(f"Error deleting permission: {e}")
+            raise DatabaseException
