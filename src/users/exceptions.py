00f09613d790d826a65f5174ac7155928d2ef027
Valeriya <Dzhavadova.v@yandex.ru>
2024-08-09 11:28:22 +0300

    common exception AlreadyExists

--- src/users/exceptions.py
+++
@@ -1,7 +0,0 @@
-from fastapi import HTTPException, status
-
-
-class UserAlreadyExists(HTTPException):
-
-    def __init__(self, detail="User already exist") -> None:
-        super().__init__(status_code=status.HTTP_409_CONFLICT, detail=detail)
